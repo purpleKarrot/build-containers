6f07955e406ee1a3446675ec0010bbc270fe458f
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-01-17 22:56:06 +0100

    iwyu: move to clang-3.9

--- clang-3.9/toolchain.cmake
+++ clang-3.9/toolchain.cmake
@@ -7,3 +7,9 @@ set(CMAKE_CXX_FLAGS_DEBUG_INIT "-fsanitize=address -fno-omit-frame-pointer")
 if(EXISTS "/source/.clang-tidy")
   set(CMAKE_CXX_CLANG_TIDY "/usr/bin/clang-tidy-3.9" CACHE INTERNAL "" FORCE)
 endif()
+
+if(EXISTS "/source/.iwyu.imp")
+  set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
+    "include-what-you-use;-Xiwyu;--mapping_file=/source/.iwyu.imp"
+    CACHE INTERNAL "" FORCE)
+endif()

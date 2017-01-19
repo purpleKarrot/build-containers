a76b484fa16e9b7594e6d42b5c95be99e7695b21
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-01-19 16:01:19 +0100

    fix arm-linux-gnueabihf

--- arm-linux-gnueabihf/toolchain.cmake
+++ arm-linux-gnueabihf/toolchain.cmake
@@ -5,9 +5,6 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER   "/usr/bin/arm-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++")
 
-set(CMAKE_FIND_ROOT_PATH "/opt/qt5")
-set(CMAKE_PREFIX_PATH "/opt/qt5")
-
 # set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 # set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 # set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)

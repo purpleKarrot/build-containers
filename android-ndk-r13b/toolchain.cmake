bb1bd7a33d349505d811acb2064dfb0b67c7ad32
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-01-20 22:51:05 +0100

    fix Android toolchain

--- android-ndk-r13b/toolchain.cmake
+++ android-ndk-r13b/toolchain.cmake
@@ -1,9 +1,8 @@
-include("/opt/android-ndk/build/cmake/android.toolchain.cmake")
+set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN "/opt/android-ndk")
 
 if(EXISTS "/source/.clang-tidy")
-  set(CMAKE_CXX_CLANG_TIDY
-    "${ANDROID_LLVM_TOOLCHAIN_PREFIX}clang-tidy${ANDROID_TOOLCHAIN_SUFFIX}"
-    CACHE INTERNAL "" FORCE)
+  set(CMAKE_CXX_CLANG_TIDY "/opt/android-ndk/bin/clang-tidy" CACHE INTERNAL "" FORCE)
 endif()
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-arm)

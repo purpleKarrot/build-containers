74fb742ed27a93a6e04caaebff6b908bad68eac2
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-01-23 00:23:21 +0100

    mingw: add rc compiler

--- mingw-w64-x86-64/toolchain.cmake
+++ mingw-w64-x86-64/toolchain.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_C_COMPILER   "/usr/bin/x86_64-w64-mingw32-gcc-win32")
-set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-w64-mingw32-g++-win32")
+set(CMAKE_C_COMPILER   "/usr/bin/x86_64-w64-mingw32-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-w64-mingw32-g++")
+set(CMAKE_RC_COMPILER  "/usr/bin/x86_64-w64-mingw32-windres")
 
-set(CMAKE_CROSSCOMPILING_EMULATOR "/usr/bin/wine64-stable")
+set(CMAKE_CROSSCOMPILING_EMULATOR "/usr/bin/wine64")

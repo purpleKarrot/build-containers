d2116b1b0de8e33c3daad5430fb04c7fdafe8333
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-01-23 20:32:11 +0100

    clang: don't enable sanitizers by default

--- clang-3.8/toolchain.cmake
+++ clang-3.8/toolchain.cmake
@@ -1,9 +1,3 @@
-set(CMAKE_C_COMPILER   "/usr/bin/clang-3.8")
-set(CMAKE_CXX_COMPILER "/usr/bin/clang++-3.8")
-
-set(CMAKE_C_FLAGS_DEBUG_INIT   "-fsanitize=address -fno-omit-frame-pointer")
-set(CMAKE_CXX_FLAGS_DEBUG_INIT "-fsanitize=address -fno-omit-frame-pointer")
-
 if(EXISTS "/source/.clang-tidy")
   set(CMAKE_CXX_CLANG_TIDY "/usr/bin/clang-tidy-3.8" CACHE INTERNAL "" FORCE)
 endif()

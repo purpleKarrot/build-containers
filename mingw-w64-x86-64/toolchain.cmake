0da47c2cf2deb314f682ec5f5f6eef62706bdd64
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-03-09 09:34:59 +0100

    fix wineprefix

--- mingw-w64-x86-64/toolchain.cmake
+++
@@ -1,7 +0,0 @@
-set(CMAKE_SYSTEM_NAME Windows)
-
-set(CMAKE_C_COMPILER   "/usr/bin/x86_64-w64-mingw32-gcc")
-set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-w64-mingw32-g++")
-set(CMAKE_RC_COMPILER  "/usr/bin/x86_64-w64-mingw32-windres")
-
-set(CMAKE_CROSSCOMPILING_EMULATOR "/usr/bin/wine64")

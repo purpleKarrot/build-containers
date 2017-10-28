4545b4f4a128dd24075349d4ad942b44a017f3fb
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-10-28 17:29:24 +0200

    mingw: use toolchain file

---
+++ mingw-w64-i686/toolchain.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_C_COMPILER   "/usr/bin/i686-w64-mingw32-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/i686-w64-mingw32-g++")
+set(CMAKE_RC_COMPILER  "/usr/bin/i686-w64-mingw32-windres")
+
+set(CMAKE_CROSSCOMPILING_EMULATOR "/usr/bin/wine")

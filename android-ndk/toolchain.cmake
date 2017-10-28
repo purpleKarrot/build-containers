db4d01f55c1a252b94e8b368e616a04ced256dfa
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-10-28 17:26:12 +0200

    don't set CMAKE_CXX_CLANG_TIDY

--- android-ndk/toolchain.cmake
+++ android-ndk/toolchain.cmake
@@ -1,8 +1,4 @@
 set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_ANDROID_NDK "/opt/ndk")
 
-# if(EXISTS "/source/.clang-tidy")
-#   set(CMAKE_CXX_CLANG_TIDY "/opt/android-ndk/bin/clang-tidy" CACHE INTERNAL "" FORCE)
-# endif()
-
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-arm)
--- clang-3.9/toolchain.cmake
+++
@@ -1,9 +0,0 @@
-if(EXISTS "/source/.clang-tidy")
-  set(CMAKE_CXX_CLANG_TIDY "/usr/bin/clang-tidy-3.9" CACHE INTERNAL "" FORCE)
-endif()
-
-if(EXISTS "/source/.iwyu.imp")
-  set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
-    "include-what-you-use;-Xiwyu;--mapping_file=/source/.iwyu.imp"
-    CACHE INTERNAL "" FORCE)
-endif()
